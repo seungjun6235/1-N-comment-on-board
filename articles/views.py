841d28662205983cd0d41fc102b7c687e89b277a
seungjun6235 <58593578+seungjun6235@users.noreply.github.com>
2023-11-06 14:30:56 +0900

    Comment delete 기능 구현

--- articles/views.py
+++ articles/views.py
@@ -70,3 +70,12 @@ def comment_create(request, article_id):
         comment.save()
 
         return redirect('articles:detail',id=article_id)
+    
+
+def comment_delete(request, article_id, id):
+    comment = Comment.objects.get(id=id)
+    comment.delete()
+
+
+    return redirect('articles:detail',id=article_id)
+    
