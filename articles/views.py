4be62146663ff0cd1ef48ae7c0d363a5a9a6d771
seungjun6235 <58593578+seungjun6235@users.noreply.github.com>
2023-11-06 10:56:14 +0900

    Create 기능 구현

--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Article 
+from .forms import ArticleForm
 
 # Create your views here.
 
@@ -11,3 +12,27 @@ def index(request):
     }
 
     return render(request,'index.html',context)
+
+def detail(request,id):
+    article = Article.objects.get(id=id)
+
+    context={
+        'article' : article
+    }
+
+    return render(request,'detail.html',context)
+
+def create(request):
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            article = form.save()
+            return redirect('articles:detail', id= article.id)
+    else:
+        form = ArticleForm()
+
+    context ={
+        'form': form
+    }
+
+    return render(request, 'form.html', context)
